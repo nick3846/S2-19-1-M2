49ecd9bfce29d835d01ecd89dfa1c9dcb7148e6f
Daniel Valsby-Koch <dava@aspit.dk>
2019-03-24 23:24:36 +0100

    Finished code base for the exercise. Got the start of a meem generator

---
+++ S2.AspNet.Repetition/S2.AspNet.Repetition/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace S2.AspNet.Repetition.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public int ImageSelected { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MemeText { get; set; } = "";
+        public string SelectedImageUrl { get; set; }
+        public void OnGet()
+        {
+            SelectedImageUrl = "/img/meme1.png";
+            
+        }
+    }
+}
